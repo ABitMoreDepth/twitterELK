64f9cda61d50a130e1c6c044cada59b45d9b9c04
Sam Martin <nivekkas@gmail.com>
2018-11-12 17:06:40 +0000

    Add some basic log management

--- ingress/ingress/__init__.py
+++ ingress/ingress/__init__.py
@@ -1,1 +1,46 @@
+""" Initial setup for the ingress package.  Includes base logging setup
+"""
+
+import logging
+from os import getenv
+
 __version__ = '0.0.1'
+
+# Setup logging for the package.  We check here whether the user has overridden
+# the default log level.
+
+LOG_FORMAT = (
+    "%(asctime)s %(levelname)s %(thread)d - [%(module)s:%(lineno)s] (%(funcName)s):  %(message)s"
+)
+
+
+def config_logging(level='INFO'):
+    """ Noddy function that will attempt to configure logging to the user desired level
+
+    :param level: The log level name to which we should attempt to log at.
+    """
+    try:
+        if level == 'SILLY':
+            # Make everything log at debug level
+            logging.basicConfig(format=LOG_FORMAT, level=logging.DEBUG)
+            root_logger = logging.getLogger()
+            root_logger.setLevel(logging.DEBUG)
+            root_logger.debug(
+                'Configured root logger to DEBUG, every library should log verbosely'
+            )
+        else:
+            logging.basicConfig(format=LOG_FORMAT, level=logging.ERROR)
+
+            log = logging.getLogger(__name__)
+            log.setLevel(level)
+            log.debug('Configured logging to level %s', level)
+
+    except AttributeError:
+        # Default case here when user borks setting the level.
+        logging.basicConfig(format=LOG_FORMAT, level=getattr(logging, level))
+
+        log = logging.getLogger(__file__)
+        log.error('User attempted to set an invalid log level, defaulting to INFO')
+
+
+config_logging(getenv("LOGLEVEL", "INFO"))
